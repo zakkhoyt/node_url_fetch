c060e62099ffdcc3d7f4d5df4b8ab28f72cdadfd
Zakk Hoyt <zakkhoyt@hatchbaby.com>
2025-04-16 04:30:59 -0600

    Capturing request in a variable

---
+++ amazon_tools.js
@@ -0,0 +1,58 @@
+// amazon_tools.js
+// Reference: https://stackoverflow.com/a/5801971
+// ========
+module.exports = {
+    extractASIN: function (url) {
+        try {
+            let result = url.match(/^(.*https:\/\/.*\/dp\/)(.*)(\?.*)$/);
+            console.log("amazon-debug 30");  
+      
+            console.log("amazon-urlRegex[0]: " + result[0]);
+            console.log("amazon-urlRegex[1]: " + result[1]);
+            console.log("amazon-urlRegex[2]: " + result[2]);
+            console.log("amazon-urlRegex[3]: " + result[3]);
+            console.log("amazon-debug 32");
+            
+            if ( result == null ){
+              console.log("amazon-debug 31");
+              console.log("amazon-urlRegex: null");
+            } else {
+              for(var i = 0; i < result.length; ++i){
+                console.log("amazon-urlRegex[i]: i: " + i + " result[i]" + result[i]);
+              }
+        
+        
+              console.log("amazon-urlRegex[0]: " + result[0]);
+              console.log("amazon-urlRegex[1]: " + result[1]);
+              console.log("amazon-urlRegex[2]: " + result[2]);
+              console.log("amazon-urlRegex[3]: " + result[3]);
+              console.log("amazon-debug 32");
+            }
+            console.log("amazon-debug 33");
+        
+        
+        
+        
+            let asid = result[2];
+            console.log("amazon-debug 50");
+        
+            if ( asid == null ){
+              // some_variable is either null or undefined
+              console.log("amazon-asid: null|undefined");
+              return null;
+            } else {
+              console.log("amazon-asid: " + asid);
+              return asid;
+            }
+          }
+          catch(err) {      
+            console.log("amazon-debug [ERROR] err.name: " + err.name + " err.message: " + err.message);
+            console.error("amazon-debug [ERROR] err.name: " + err.name + " err.message: " + err.message);
+            debugger;
+          } 
+    }
+    //,
+    // bar: function () {
+    //   // whatever
+    // }
+  };
--- fetch_html.js
+++ fetch_html.js
@@ -1,7 +1,12 @@
 
 const https = require('https');
 const process = require('node:process');
-//import process from 'node:process';
+
+const amazon_tools = require('./amazon_tools');
+console.log(`typeof amazon_tools.extractASIN: ${typeof amazon_tools.extractASIN}`); // => 'function'
+console.log(`typeof amazon_tools.bar: ${typeof amazon_tools.bar}`); // => 'function'
+
+
 
 // const url = "https://duckduckgo.com/";
 // const url = "https://www.amazon.com/dp/B0DP5BQTRV";
@@ -12,16 +17,38 @@ const process = require('node:process');
 let url = "https://www.amazon.com/dp/B0DP5BQTRV";
 console.log(`Will fetch html for url: ${url}`)
 
+var count = 0;
 https.get(
     url, 
-    function(res) {
-        console.log(res.statusCode);
+    function(response) {
+        console.log(response.statusCode);
         
-        res.setEncoding('utf8');
-        res.on('data', function(data) {
-            // console.log(data);
-            process.stdout.write(data);
-        });
+        response.setEncoding('utf8');
+        // response.on('data', function(data) {
+        //     // console.log(data);
+        //     process.stdout.write(data);
+            
+        //     console.log(`count: ${count}`);
+        //     count += 1;
+        // });
+
+        // Append chunk of the request data here
+        var finalSourceCode = ''
+        response.on('data', function(chunk) {
+            console.log(`Received chunk: ${count}`);
+            count += 1;
+            finalSourceCode += chunk
+        }); 
+
+        // https://nodejs.org/api/stream.html#stream_event_end
+        response.on('end', function() {
+            debugger;
+            // console.log(`finalSourceCode: ${finalSourceCode}`)
+            process.stdout.write(finalSourceCode);
+            dapi.message.send(finalSourceCode)
+        }); 
+
+        
     }
 )
 .on(
